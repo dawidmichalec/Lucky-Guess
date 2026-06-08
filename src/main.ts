9fd7cf154d80f71834bd0dd0ba819004069d3809
dawidmichalec <michalecdawid95@gmail.com>
2026-06-08 16:06:15 +0200

    Added placeholder writings for Balance, Bet, Mode and Multipliers

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { text } from 'motion/react-client';
-import { Application, Container, Graphics, Text, Sprite, Assets } from 'pixi.js';
+import { Application, Container, Graphics, Text, Sprite, Assets, BlurFilter } from 'pixi.js';
 
 (async () => {
     const app = new Application();
@@ -16,10 +16,82 @@ import { Application, Container, Graphics, Text, Sprite, Assets } from 'pixi.js'
     app.canvas.style.top = '0';
     app.canvas.style.left = '0';
 
-    const texture = await Assets.load('./luckygame assets/images/luckyguess background.png');
-    const background = new Sprite(texture);
+    const backgroundImage = await Assets.load('./luckygame assets/images/luckyguess background.png');
+    const background = new Sprite(backgroundImage);
     background.width = app.screen.width;
     background.height = app.screen.height;
     app.stage.addChild(background);
+
+    // Font Face
+
+    const font = new FontFace( 'Anton', 'url(/fonts/Anton-Regular.ttf)' ); 
+    await font.load(); 
+    document.fonts.add(font); 
+    await document.fonts.ready;
     
+    //MODE Display
+
+    const modeText = new Text({
+      text: 'MODE',
+      style: {
+        fontFamily: 'Anton',
+        fontSize: 32,
+        fill: 0xffd700,
+      }
+    });
+
+    modeText.position.set(1380, 185);
+
+    app.stage.addChild(modeText);
+
+    // Mulitpliers Display
+
+    const multipliersText = new Text({
+      text: 'Multiplier',
+      style: {
+        fontFamily: 'Anton',
+        fontSize: 32,
+        fill: 0xffde59,
+      }
+    });
+
+    multipliersText.x = 1150;
+    multipliersText.y = 185;
+
+    app.stage.addChild(multipliersText);
+
+    // BALANCE Text
+
+    const balanceWordText = new Text({
+      text: 'BALANCE',
+      style: {
+        font: 'Open Sans',
+        fontSize: '24',
+        fontWeight: 'bold',
+        fill: 0xffd700,
+      }
+    });
+
+    balanceWordText.x = 430;
+    balanceWordText.y = 665;
+
+    app.stage.addChild(balanceWordText);
+
+    // BET Text
+
+    const betText = new Text({
+      text: 'BET',
+      style: {
+        font: 'Open Sans',
+        fontSize: 24,
+        fontWeight: 'bold',
+        fill: 0xffd700,
+      }
+    });
+
+    betText.x = 1050;
+    betText.y = 665;
+
+    app.stage.addChild(betText);
+
 })();
